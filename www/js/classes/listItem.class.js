0633c201a81fdf7729aadbbab37bad6831897928
Marcus Thelin <marcusthelin97@gmail.com>
2017-12-27 12:26:41 +0100

    Method to add class disabled to buttons

--- www/js/classes/listItem.class.js
+++ www/js/classes/listItem.class.js
@@ -9,30 +9,6 @@ class ListItem extends Base{
 
   template(){
     if(this.list == app.toDoList){
-
-      if(this === app.toDoList.items[0]){
-        return `<div>
-                  <li class="list-group-item" data-item="${this.item}">
-                    ${this.item}
-                    <input class="form-check-input checkbox" type="checkbox" value="">
-                    <div class="moveArrows">
-                      <button class="btn btn-sm btnDown"><i class="fas fa-angle-down arrow btnDown"></i></button>
-                      <button class="btn btn-sm btnUp disabled"><i class="disabled fas fa-angle-up arrow btnUp"></i></button>
-                    </div>
-                  </li>
-                </div>`;
-      } else if(this === app.toDoList.items[(app.toDoList.items.length - 1)]){
-        return `<div>
-                  <li class="list-group-item" data-item="${this.item}">
-                    ${this.item}
-                    <input class="form-check-input checkbox" type="checkbox" value="">
-                    <div class="moveArrows">
-                      <button class="btn btnDown btn-sm disabled"><i class="disabled fas fa-angle-down arrow btnDown"></i></button>
-                      <button class="btn btnUp btn-sm"><i class="fas fa-angle-up arrow btnUp"></i></button>
-                    </div>
-                  </li>
-                </div>`;
-      } else{
         return `<div>
                   <li class="list-group-item" data-item="${this.item}">
                     ${this.item}
@@ -43,17 +19,12 @@ class ListItem extends Base{
                     </div>
                   </li>
                 </div>`;
-      }
-
-
-    } else if(this.list == app.doneList){
+      } else if(this.list == app.doneList){
       return `<div>
                 <li class="list-group-item" data-item="${this.item}">
                   ${this.item}<button class="btn btn-secondary float-md-right removeBtn" type="button">X</button>
                 </li>
               </div>`;
     }
-
   }
-
 }
